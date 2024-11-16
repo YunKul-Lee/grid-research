86358d175fae9484c00103a1ba0d43d7dfcdc68d
yklee <gerion@gmail.com>
2024-11-16 21:53:07 +0900

    .

--- src/components/TabulatorGrid.vue
+++ src/components/TabulatorGrid.vue
@@ -3,34 +3,31 @@ import { useTemplateRef, ref, reactive, onMounted } from 'vue';
 import { TabulatorFull as Tabulator } from 'tabulator-tables';
 import 'tabulator-tables/dist/css/tabulator.css'
 
+// 샘플 데이터
+const data1 = [
+  {"name": 'Arnulfo Hettinger', "progress": 57, "gender": "female", "rating": "5", "col": "violet", "dob": "16/10/2024"},
+  {"name": 'Watson Greenholt', "progress": 86, "gender": "male", "rating": "5", "col": "yellow", "dob": "30/07/2024"},
+  {"name": 'Adolphus Volkman', "progress": 43, "gender": "male", "rating": "4", "col": "indigo", "dob": "15/07/2024"},
+  {"name": 'Hayley Spinka', "progress": 86, "gender": "female", "rating": "2", "col": "silver", "dob": "05/04/2024"},
+  {"name": 'Yvette Walker', "progress": 67, "gender": "female", "rating": "5", "col": "azure", "dob": "03/01/2024"},
+  {"name": 'Keagan Davis', "progress": 64, "gender": "male", "rating": "5", "col": "ivory", "dob": "19/06/2024"},
+  {"name": 'Anastasia Towne', "progress": 3, "gender": "female", "rating": "1", "col": "lavender", "dob": "12/05/2024"},
+  {"name": 'Dr. Serena White', "progress": 32, "gender": "female", "rating": "5", "col": "violet", "dob": "14/04/2024"},
+  {"name": 'Casper Gottlieb', "progress": 38, "gender": "female", "rating": "2", "col": "yellow", "dob": "10/06/2024"}
+]
+
 // const table = ref(null);
 const basicTable = useTemplateRef('basic-table')
 const basicTabulator = ref(null);
-const basicTableData = reactive([
-  {"name": 'Arnulfo Hettinger', "progress": 57, "gender": "female", "rating": "5", "col": "violet", "dob": "16/10/2024"},
-  {"name": 'Watson Greenholt', "progress": 86, "gender": "male", "rating": "5", "col": "yellow", "dob": "30/07/2024"},
-  {"name": 'Adolphus Volkman', "progress": 43, "gender": "male", "rating": "4", "col": "indigo", "dob": "15/07/2024"},
-  {"name": 'Hayley Spinka', "progress": 86, "gender": "female", "rating": "2", "col": "silver", "dob": "05/04/2024"},
-  {"name": 'Yvette Walker', "progress": 67, "gender": "female", "rating": "5", "col": "azure", "dob": "03/01/2024"},
-  {"name": 'Keagan Davis', "progress": 64, "gender": "male", "rating": "5", "col": "ivory", "dob": "19/06/2024"},
-  {"name": 'Anastasia Towne', "progress": 3, "gender": "female", "rating": "1", "col": "lavender", "dob": "12/05/2024"},
-  {"name": 'Dr. Serena White', "progress": 32, "gender": "female", "rating": "5", "col": "violet", "dob": "14/04/2024"},
-  {"name": 'Casper Gottlieb', "progress": 38, "gender": "female", "rating": "2", "col": "yellow", "dob": "10/06/2024"},
-]);
+const basicTableData = reactive([...data1]);
 
 const fitDataFillTable = useTemplateRef('fit-data-fill-table');
 const fitDataFillTableTabulator = ref(null);
-const fitDataFillTableData = reactive([
-  {"name": 'Arnulfo Hettinger', "progress": 57, "gender": "female", "rating": "5", "col": "violet", "dob": "16/10/2024"},
-  {"name": 'Watson Greenholt', "progress": 86, "gender": "male", "rating": "5", "col": "yellow", "dob": "30/07/2024"},
-  {"name": 'Adolphus Volkman', "progress": 43, "gender": "male", "rating": "4", "col": "indigo", "dob": "15/07/2024"},
-  {"name": 'Hayley Spinka', "progress": 86, "gender": "female", "rating": "2", "col": "silver", "dob": "05/04/2024"},
-  {"name": 'Yvette Walker', "progress": 67, "gender": "female", "rating": "5", "col": "azure", "dob": "03/01/2024"},
-  {"name": 'Keagan Davis', "progress": 64, "gender": "male", "rating": "5", "col": "ivory", "dob": "19/06/2024"},
-  {"name": 'Anastasia Towne', "progress": 3, "gender": "female", "rating": "1", "col": "lavender", "dob": "12/05/2024"},
-  {"name": 'Dr. Serena White', "progress": 32, "gender": "female", "rating": "5", "col": "violet", "dob": "14/04/2024"},
-  {"name": 'Casper Gottlieb', "progress": 38, "gender": "female", "rating": "2", "col": "yellow", "dob": "10/06/2024"},
-])
+const fitDataFillTableData = reactive([...data1])
+
+const frozenTable = useTemplateRef('frozen-table')
+const frozenTabulator = ref(null);
+const frozenTabulatorData = reactive([...data1])
 
 onMounted(() => {
   // 기본
@@ -75,6 +72,19 @@ onMounted(() => {
       {title:"Date Of Birth", field:"dob", hozAlign:"center"},
     ]
   })
+
+  frozenTabulator.value = new Tabulator(frozenTable.value, {
+    height: '300px',
+    data: frozenTabulatorData,
+    columns: [
+      {title:"Name", field:"name", frozen: true},
+      {title:"Progress", field:"progress", sorter:"number"},
+      {title:"Gender", field:"gender"},
+      {title:"Rating", field:"rating"},
+      {title:"Favourite Color", field:"col"},
+      {title:"Date Of Birth", field:"dob", hozAlign:"center"},
+    ]
+  })
 })
 </script>
 
@@ -88,7 +98,9 @@ onMounted(() => {
     <div ref="fit-data-fill-table"></div>
     <hr />
 
-    <h2>Responsive Layout</h2>
+    <h2>틀고정</h2>
+    <div ref="frozen-table"></div>
+    <hr />
   </div>
 </template>
 
